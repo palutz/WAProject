1cebfbdcbf529c20887de29e04d520d888e14e68
palutz <stefano.paluello@gmail.com>
2015-05-13 19:29:03 +0100

    file controller

---
+++ WAProject/Controllers/UploadFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WAProject.Controllers
+{
+    public class UploadFile : Controller
+    {
+		public ActionResult Index(HttpPostedFileBase file)
+		{	
+			if (file.ContentLength > 0) {
+				var fileName = Path.GetFileName(file.FileName);
+				var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+				file.SaveAs(path);
+			}
+
+			return RedirectToAction("Index");
+		}
+
+        
+    }
+}
---
+++ WAProject/Global.asax
@@ -0,0 +1,2 @@
+﻿<%@ Application Inherits="WAProject.MvcApplication" %>
+
